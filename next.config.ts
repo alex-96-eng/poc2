59a32bd98423cdc7c480c2b02db6f14afffc98a4
Alex <alexisherwood201@gmail.com>
2025-09-08 15:12:49 +0900

    disable linting

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
+// next.config.ts
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  eslint: {
+    // Skip ESLint in `next build`
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    // Skip TS type errors in `next build`
+    ignoreBuildErrors: true,
+  },
 };
 
 export default nextConfig;
